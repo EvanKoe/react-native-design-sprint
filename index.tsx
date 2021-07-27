d20ab1ffd7e7d1cd3f759f5298a6450ac727bfa9
Evan Koehler <evan.koehler@epitech.eu>
2021-07-27 11:50:34 +0200

    [add] index.tsx

---
+++ index.tsx
@@ -0,0 +1,21 @@
+import { Clickable } from './components/Clickable';
+import { Input } from './components/Input';
+import { LoginForm } from './components/Form';
+import { Layout } from './components/Layout';
+import colors from './resources/colors';
+import { Spacer } from './components/Spacer';
+import { Avatar } from './components/Avatar';
+import { DropDown } from './components/DropDown';
+import { Title } from './components/Title';
+
+export default {
+  Clickable,
+  Input,
+  LoginForm,
+  Layout,
+  colors,
+  Spacer,
+  Avatar,
+  DropDown,
+  Title
+};
