fcb9c90c6f727f4e826b9f4fc3e45b1fd7178a8c
Evan Koehler <evan.koehler@epitech.eu>
2021-07-28 11:35:19 +0200

    added placeholder options for LoginForm

--- src/Form.tsx
+++ src/Form.tsx
@@ -5,20 +5,24 @@ import { View, Text, StyleSheet, ViewStyle, StyleProp, TextStyle } from 'react-n
 import colors from './colors';
 
 interface Props {
-  title?: string;                           // title text
-  mainStyle?: StyleProp<ViewStyle>;         // style to be applied to the main container
-  titleStyle?: StyleProp<TextStyle>;        // style to be applied to the title
-  buttonColor?: string;                     // color for the button
-  buttonType?: string;                      // primary/secondary/disabled : Clickable options
-  buttonStyle?: StyleProp<ViewStyle>;       // style to be applied to the submit button
-  buttonSticky?: string;                    // left/center/right/none : make the button stick horizontally
-  buttonShadow?: string;                    // none/right-top ... adds a little shadow under the button (with borders)
-  buttonShadowColor?: string;               // color of the button's shadow
-  style?: StyleProp<ViewStyle>;             // style to be applied to the main layout
-  callback?: (object: any) => void;         // function called when submit clicked
-  shadow?: string;                          // none/left-bottom/right-top ... Displays a cheating shadow made with borders
-  shadowColor?: string;                     // color of the shadow
-  borderRadius?: number;                    // border radius (in px) for the main layout, inputs and buttons
+  title?: string;                              // title text
+  mainStyle?: StyleProp<ViewStyle>;            // style to be applied to the main container
+  titleStyle?: StyleProp<TextStyle>;           // style to be applied to the title
+  buttonColor?: string;                        // color for the button
+  buttonType?: string;                         // primary/secondary/disabled : Clickable options
+  buttonStyle?: StyleProp<ViewStyle>;          // style to be applied to the submit button
+  buttonSticky?: string;                       // left/center/right/none : make the button stick horizontally
+  buttonShadow?: string;                       // none/right-top ... adds a little shadow under the button (with borders)
+  buttonShadowColor?: string;                  // color of the button's shadow
+  style?: StyleProp<ViewStyle>;                // style to be applied to the main layout
+  callback?: (object: any) => void;            // function called when submit clicked
+  shadow?: string;                             // none/left-bottom/right-top ... Displays a cheating shadow made with borders
+  shadowColor?: string;                        // color of the shadow
+  borderRadius?: number;                       // border radius (in px) for the main layout, inputs and buttons
+  placeholderTop?: string;                     // text to be displayed in the top placeholder
+  placeholderTopStyle?: StyleProp<TextStyle>   // style to be applied to the top placeholder
+  placeholderBottom?: string;                  // text to be displayed in the bottom placeholder
+  placeholderBottomStyle?: StyleProp<TextStyle>// style to be applied to the bottom placeholder
 }
 
 export const LoginForm: FC<Props> = ({
@@ -35,7 +39,11 @@ export const LoginForm: FC<Props> = ({
   shadow = 'none',
   shadowColor = colors.grey60,
   callback = (object: any) => console.log('Form callback missing. Please add one.'),
-  borderRadius = 15
+  borderRadius = 15,
+  placeholderTop = 'Email',
+  placeholderTopStyle = {},
+  placeholderBottom = 'Password',
+  placeholderBottomStyle = {}
 }) => {
   const styles = StyleSheet.create({
     container: {
@@ -70,11 +78,21 @@ export const LoginForm: FC<Props> = ({
         {(title !== '') && (
           <Text style={[styles.title, titleStyle]}> {title} </Text>
         )}
-        <Input type='email' required noRegex onCharTyped={(e) => setMail(e)}
-          style={{ borderRadius: borderRadius }}
+        <Input
+          type='email'
+          required
+          noRegex
+          onCharTyped={(e) => setMail(e)}
+          style={[{ borderRadius: borderRadius }, placeholderTopStyle]}
+          placeholder={placeholderTop}
         />
-        <Input type='password' required noRegex onCharTyped={(e) => setPsw(e)}
-          style={{ borderRadius: borderRadius }}
+        <Input
+          type='password'
+          required
+          noRegex
+          onCharTyped={(e) => setPsw(e)}
+          style={[{ borderRadius: borderRadius }, placeholderBottomStyle]}
+          placeholder={placeholderBottom}
         />
         <Clickable
           shadow={buttonShadow}
